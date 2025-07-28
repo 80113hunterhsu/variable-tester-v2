8715c626fea56806b7b9ebd8b49f79a271a454ab
Hunter Hsu <80113hunterhsu@gmail.com>
2025-07-29 00:36:29 +0800

    component(PageContainer): create PageContainer component

---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,47 @@
+// node modules
+import React from "react";
+
+// components
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+// styles
+import "./PageContainer.css";
+
+// types
+import { NavbarLink } from "../types/Navbar/NavbarTypes";
+
+// PageContainerProps 型別
+type PageContainerProps = {
+    navbarLinks: NavbarLink[];
+    children: React.ReactNode;
+};
+
+export default function PageContainer({
+    navbarLinks,
+    children,
+}: PageContainerProps) {
+    const classes: string[] = [
+        "container-fluid",
+        "d-flex",
+        "flex-column",
+        "m-0",
+        "px-4",
+        "py-0",
+        "gap-4"
+    ];
+    return (
+        <div id="page-container" className={classes.join(" ")}>
+            {/* Navbar */}
+            <Navbar navbarLinks={navbarLinks} />
+
+            {/* 內容區塊 */}
+            <div id="content-container" className="px-4 py-3 d-flex flex-center">
+                {children}
+            </div>
+
+            {/* Footer */}
+            <Footer />
+        </div>
+    );
+}
