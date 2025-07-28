ba0b85b3fb4e3edc89cc09e523c8fb9b1efea27f
Hunter Hsu <80113hunterhsu@gmail.com>
2025-07-29 00:22:50 +0800

    component(Footer): create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+export default function Footer() {
+    const classes: string[] = [
+        "text-center",
+        "text-muted",
+        "px-4",
+        "py-3",
+        "mb-4",
+        "element",
+    ];
+    return (
+        <footer className={classes.join(" ")}>
+            &copy; {new Date().getFullYear()} Variable Tester | <a href="https://www.hunterhsu.net/" target="_blank">Hunter Hsu</a>
+        </footer>
+    );
+}
