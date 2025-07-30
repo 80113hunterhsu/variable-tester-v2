c53ef77cca1ea631f117ae4955185c84df119a08
Hunter Hsu <80113hunterhsu@gmail.com>
2025-07-30 20:43:42 +0800

    feat: Add CSS styles, and changed the loading order of the css file

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
