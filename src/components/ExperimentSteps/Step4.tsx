9d0d93d9003bc605da77ae2ead2c99595652ba80
Hunter Hsu <80113hunterhsu@gmail.com>
2025-08-10 15:06:44 +0800

    feat(ExperimentSteps): Step 4
    Also modified CSP rules

---
+++ src/components/ExperimentSteps/Step4.tsx
@@ -0,0 +1,48 @@
+// node modules
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+// helpers
+import { checkRequiredFields } from "../../helpers/ExperimentStepsHelper";
+
+function renderContent(data: { [key: string]: any }) {
+    return (
+        <video controls controlsList="nodownload" className="p-0 rounded-4">
+            <source src={URL.createObjectURL(data.video)} type="video/mp4" />
+        </video>
+    );
+}
+
+/**
+ * Step 4: Preview video and prepare for the start of the experiment
+ */
+export default function Step4({ data }: { data: { [key: string]: any } }) {
+    const nav = useNavigate();
+    const [content, setContent] = useState<JSX.Element | null>(null);
+    useEffect(() => {
+        if (!checkRequiredFields(["subjectName", "variableName", "video"], data)) {
+            nav("/experiment", { replace: true });
+        } else {
+            setContent(renderContent(data));
+        }
+    }, []);
+    return (
+        <div className="row col-8 flex-center gap-5">
+            {content}
+            <div className="d-flex flex-center gap-3">
+                <Link
+                    className="btn btn-outline-secondary btn-lg"
+                    to={"/experiment/step/3"}
+                >
+                    Back
+                </Link>
+                <Link
+                    className="btn btn-outline-primary btn-lg"
+                    to={"/experiment/step/5"}
+                >
+                    Next
+                </Link>
+            </div>
+        </div>
+    );
+}
