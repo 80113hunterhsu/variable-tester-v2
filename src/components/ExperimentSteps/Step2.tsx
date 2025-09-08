7a13ef980fa5587b7d293e4d34b0493634e1d14f
Hunter Hsu <80113hunterhsu@gmail.com>
2025-09-08 21:39:12 +0800

    feat(Step2): add enter key binding

--- src/components/ExperimentSteps/Step2.tsx
+++ src/components/ExperimentSteps/Step2.tsx
@@ -1,9 +1,9 @@
 // node modules
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 // helpers
-import { checkRequiredFields } from "../../helpers/ExperimentStepsHelper";
+import { checkRequiredFields, bindEnterKey } from "../../helpers/ExperimentStepsHelper";
 
 function renderContent(data: { [key: string]: any }) {
     return (
@@ -36,6 +36,8 @@ function renderContent(data: { [key: string]: any }) {
 export default function Step2({ data }: { data: { [key: string]: any } }) {
     const nav = useNavigate();
     const [content, setContent] = useState<JSX.Element | null>(null);
+    const nextBtnRef = useRef<HTMLAnchorElement>(null);
+    bindEnterKey(nextBtnRef);
     useEffect(() => {
         if (!checkRequiredFields(["subjectName", "variableName"], data)) {
             nav("/experiment", { replace: true });
@@ -56,6 +58,7 @@ export default function Step2({ data }: { data: { [key: string]: any } }) {
                 <Link
                     to="/experiment/step/3"
                     className="btn btn-outline-primary btn-lg"
+                    ref={nextBtnRef}
                 >
                     Next
                 </Link>
