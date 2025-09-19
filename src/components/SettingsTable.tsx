12a042ab03e79aa6d5886b94b59a1002811e447e
Hunter Hsu <80113hunterhsu@gmail.com>
2025-09-19 22:40:25 +0800

    refactor(Settings): separated settings value table from Settings Page, in order to reuse component

---
+++ src/components/SettingsTable.tsx
@@ -0,0 +1,123 @@
+// node modules
+import { useRef, RefObject } from "react";
+import { Form } from "react-bootstrap";
+
+// helpers
+import { generateElementKey } from "../helpers/Element";
+import { getSettingOrder, getSettingKeys } from "../helpers/Settings";
+
+const autosaveTimeout = 500;
+export default function SettingsTable({
+    settings,
+    isEditable = false,
+    setSettings = () => { },
+    setSettingsChanged = () => { },
+    refUpdatedMessage = null,
+}: {
+    settings: Record<string, any>;
+    isEditable?: boolean;
+    setSettings?: (settings: Record<string, any>) => void;
+    setSettingsChanged?: (changed: boolean) => void;
+    refUpdatedMessage?: RefObject<HTMLSpanElement> | null;
+}) {
+    const refTableBody = useRef<HTMLTableSectionElement>(null);
+    const refSaveTimeout = useRef<NodeJS.Timeout | null>(null);
+    const refRangeIndicator = useRef<any>(null);
+    const settingOrder = getSettingOrder();
+    const settingKeys = getSettingKeys();
+    const handleSave = (key: string, newValue: any) => {
+        setSettings((prevSettings: Record<string, any>) => ({
+            ...prevSettings,
+            [key]: newValue,
+        }));
+        setSettingsChanged(true);
+        if (refSaveTimeout.current) {
+            clearTimeout(refSaveTimeout.current);
+        }
+        refSaveTimeout.current = setTimeout(async () => {
+            console.log(key, newValue);
+            await window.db.settings.set({ key: key, value: newValue });
+            refUpdatedMessage &&
+                refUpdatedMessage.current &&
+                refUpdatedMessage.current.classList.add("show");
+            setTimeout(() => {
+                refUpdatedMessage &&
+                    refUpdatedMessage.current &&
+                    refUpdatedMessage.current.classList.remove("show");
+            }, 1000);
+        }, autosaveTimeout);
+    };
+    const renderInput = (key: string, value: any) => {
+        const shouldDisableVolumeSetting =
+            key === "notifySoundVolume" && !settings.useNotifySound;
+        const templates: Record<string, any> = {
+            number: (key: string, value: number) => (
+                <Form.Control
+                    type="number"
+                    min={0}
+                    defaultValue={value}
+                    onChange={(e) => handleSave(key, Number(e.target.value))}
+                    disabled={!isEditable}
+                />
+            ),
+            switch: (key: string, value: boolean) => (
+                <Form.Switch
+                    className="my-1"
+                    defaultChecked={value}
+                    onChange={(e) => handleSave(key, e.target.checked)}
+                    disabled={!isEditable}
+                />
+            ),
+            range: (key: string, value: number) => (
+                <>
+                    <Form.Group className="d-flex align-items-center">
+                        <Form.Label
+                            ref={refRangeIndicator}
+                            className={`col-1 my-1 ${shouldDisableVolumeSetting && "text-muted"
+                                }`}
+                        >
+                            {value}%
+                        </Form.Label>
+                        <Form.Range
+                            min={0}
+                            max={100}
+                            defaultValue={value}
+                            onChange={(e) => {
+                                refRangeIndicator.current &&
+                                    (refRangeIndicator.current.innerHTML = `${e.target.value}%`);
+                                handleSave(key, Number(e.target.value));
+                            }}
+                            disabled={!isEditable || shouldDisableVolumeSetting}
+                        />
+                    </Form.Group>
+                </>
+            ),
+        };
+        const inputType = settingKeys[key].inputType || "number";
+        return templates[inputType](key, value);
+    };
+
+    const eleSettings = settingOrder.map((key) => {
+        const info = settingKeys[key];
+        const value = settings[key];
+        return (
+            <tr key={generateElementKey(`settings-${key}`)}>
+                <td className="align-middle">{info.description}</td>
+                <td className="align-middle">{renderInput(key, value)}</td>
+            </tr>
+        );
+    });
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col" className="col-5">
+                        Description
+                    </th>
+                    <th scope="col">Value</th>
+                </tr>
+            </thead>
+            <tbody ref={refTableBody}>{eleSettings}</tbody>
+        </table>
+    );
+}
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,113 +1,18 @@
 // node modules
-import { useState, useEffect, useRef, RefObject } from "react";
-import { Form } from "react-bootstrap";
+import { useState, useEffect, useRef } from "react";
 
 // components
 import PageContainer from "../components/PageContainer";
+import SettingsTable from "../components/SettingsTable";
 
 // helpers
 import { setSubtitle } from "../helpers/Title";
-import { generateElementKey } from "../helpers/Element";
-import { getSettingOrder, getSettingKeys } from "../helpers/Settings";
+import { getSettingKeys } from "../helpers/Settings";
 
 // styles
 import "./Settings.css";
 
-const autosaveTimeout = 500;
-const settingOrder = getSettingOrder();
 const settingKeys = getSettingKeys();
-function renderTable(
-    refTableBody: RefObject<HTMLTableSectionElement>,
-    settings: Record<string, any>,
-    setSettings: (settings: Record<string, any>) => void,
-    refSaveTimeout: any,
-    refRangeIndicator: any,
-    setSettingsChanged: (changed: boolean) => void,
-    refUpdatedMessage: RefObject<HTMLSpanElement>,
-) {
-    const handleSave = (key: string, newValue: any) => {
-        setSettings((prevSettings: Record<string, any>) => ({ ...prevSettings, [key]: newValue }));
-        setSettingsChanged(true);
-        if (refSaveTimeout.current) {
-            clearTimeout(refSaveTimeout.current);
-        }
-        refSaveTimeout.current = setTimeout(async () => {
-            console.log(key, newValue);
-            await window.db.settings.set({ key: key, value: newValue });
-            refUpdatedMessage.current && (refUpdatedMessage.current.classList.add('show'));
-            setTimeout(() => {
-                refUpdatedMessage.current && (refUpdatedMessage.current.classList.remove('show'));
-            }, 1000);
-        }, autosaveTimeout);
-    };
-    const renderInput = (key: string, value: any) => {
-        const shouldDisableVolumeSetting = key === "notifySoundVolume" && !settings.useNotifySound;
-        const templates: Record<string, any> = {
-            number: (key: string, value: number) => (
-                <Form.Control
-                    type="number"
-                    min={0}
-                    defaultValue={value}
-                    onChange={(e) => handleSave(key, Number(e.target.value))}
-                />
-            ),
-            switch: (key: string, value: boolean) => (
-                <Form.Switch
-                    className="my-1"
-                    defaultChecked={value}
-                    onChange={(e) => handleSave(key, e.target.checked)}
-                />
-            ),
-            range: (key: string, value: number) => (
-                <>
-                    <Form.Group className="d-flex align-items-center">
-                        <Form.Label ref={refRangeIndicator} className={`col-1 my-1 ${shouldDisableVolumeSetting && 'text-muted'}`}>
-                            {value}%
-                        </Form.Label>
-                        <Form.Range
-                            min={0}
-                            max={100}
-                            defaultValue={value}
-                            onChange={(e) => {
-                                refRangeIndicator.current && (refRangeIndicator.current.innerHTML = `${e.target.value}%`);
-                                handleSave(key, Number(e.target.value));
-                            }}
-                            disabled={shouldDisableVolumeSetting}
-                        />
-                    </Form.Group>
-                </>
-            ),
-        };
-        const inputType = settingKeys[key].inputType || "number";
-        return templates[inputType](key, value);
-    };
-
-    const eleSettings = settingOrder.map((key) => {
-        const info = settingKeys[key];
-        const value = settings[key];
-        console.log(key, value);
-        return (
-            <tr key={generateElementKey(`settings-${key}`)}>
-                <td className="align-middle">{info.description}</td>
-                <td className="align-middle">{renderInput(key, value)}</td>
-            </tr>
-        );
-    });
-    return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th scope="col" className="col-5">
-                        Description
-                    </th>
-                    <th scope="col">Value</th>
-                </tr>
-            </thead>
-            <tbody ref={refTableBody}>{eleSettings}</tbody>
-        </table>
-    );
-}
-
 const links = [
     { title: "Experiment", to: "/experiment" },
     { title: "Results", to: "/results" },
@@ -116,9 +21,6 @@ const links = [
 export default function Settings() {
     setSubtitle("Settings");
     const refUpdatedMessage = useRef<HTMLSpanElement>(null);
-    const refTableBody = useRef<HTMLTableSectionElement>(null);
-    const refSaveTimeout = useRef<NodeJS.Timeout | null>(null);
-    const refRangeIndicator = useRef<any>(null);
     const [eleTable, setEleTable] = useState<JSX.Element | null>(<></>);
     const [settings, setSettings] = useState<Record<string, any>>({});
     const [settingsLoaded, setSettingsLoaded] = useState(false);
@@ -148,19 +50,36 @@ export default function Settings() {
             return;
         }
         setEleTable(
-            renderTable(refTableBody, settings, setSettings, refSaveTimeout, refRangeIndicator, setSettingsChanged, refUpdatedMessage)
+            <SettingsTable
+                {...{
+                    settings,
+                    isEditable: true,
+                    setSettings,
+                    setSettingsChanged,
+                    refUpdatedMessage,
+                }}
+            />
         );
         setSettingsChanged(false);
         console.log("");
     }, [settings, settingsLoaded, settingsChanged]);
-    useEffect(() => console.log("settings changed: ", settings), [settingsChanged]);
+    useEffect(
+        () => console.log("settings changed: ", settings),
+        [settingsChanged]
+    );
 
     return (
         <PageContainer navbarLinks={links} centered={false}>
             <div className="d-flex flex-center flex-column gap-lg-4 gap-3 col-12">
                 <div className="d-flex flex-column flex-center">
                     <h1 className="text-center">Settings</h1>
-                    <span id="settingsUpdatedMessage" ref={refUpdatedMessage} className="text-muted">Settings saved.</span>
+                    <span
+                        id="settingsUpdatedMessage"
+                        ref={refUpdatedMessage}
+                        className="text-muted"
+                    >
+                        Settings saved.
+                    </span>
                 </div>
                 <div className="table-responsive col-12">{eleTable}</div>
             </div>
